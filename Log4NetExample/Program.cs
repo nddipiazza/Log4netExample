c78be8eeaad9def594d15c249941e3992c378d66
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2018-05-09 12:37:02 -0500

    add

---
+++ Log4NetExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace Log4NetExample {
+  class MainClass {
+    private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    public static void Main(string[] args) {
+      log4net.Config.XmlConfigurator.Configure(new FileInfo("log4net.config"));
+      log.Info("hi");
+    }
+  }
+}
